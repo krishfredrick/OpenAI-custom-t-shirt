bc3419b2105dd953e6791bba976d014caebc1c0b
Rajesh Kannan <krishfredrick11@gmail.com>
2023-05-22 14:09:14 +0530

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import Home from './pages/Home'
+import CanvasModel from './canvas'
+import Customizer from './pages/Customizer'
+function App() {
+  return (
+    <main className='app transition-all ease-in'>
+      <Home />
+      <CanvasModel />
+      <Customizer />
+    </main>
+  )
+}
+
+export default App
---
+++ client/src/components/AiPicker.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+
+import CustomButton from './CustomButton'
+
+
+
+function AiPicker({prompt, setPrompt, generatingImg, handleSubmit}) {
+  return (
+    <div className='apipicker-container'>
+      <textarea
+       className='apipicker-textarea'
+       placeholder='Genearte Prompt'
+       rows={5}
+       value={prompt}
+       onChange={(e)=>setPrompt(e.target.value)}
+      />
+      <div className="flex flex-wrap gap-3">
+        {generatingImg?(
+          <CustomButton 
+            type='outline'
+            title='Asking AI...'
+            customStyle='text-xs'
+          />
+        ):(
+          <>
+            <CustomButton 
+            type='outline'
+            title='AI logo'
+            customStyle='text-xs'
+            handleClick={()=>handleSubmit('logo')}
+          />
+           <CustomButton 
+            type='filled'
+            title='AI Full'
+            customStyle='text-xs'
+            handleClick={()=>handleSubmit('full')}
+          />
+          </>
+        )}
+      </div>
+      AiPicker
+    </div>
+  )
+}
+
+export default AiPicker
